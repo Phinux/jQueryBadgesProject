a51b909a2d7cf07d2edd7fcaa2920aabaa32482f
atoonen <atoonen@NB-ATOONEN>
2017-05-11 22:44:03 +0200

    tweede pogin

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,14 +2,27 @@ $(function() {
 
     // your code will go here
     
-
-    //Make an Ajax request to the Report Card URL you found on CodeSchool.com - Set the dataType as jsonp - Pass a success callback We made a sample Ajax request available to help you with this task.
-
     $.ajax({
-        url: "https://www.codeschool.com/users/Phinux", success: function (result) {
-
-
+        url: 'https://www.codeschool.com/users/Phinux',
+        dataType: 'jsonp',
+        success: function (response) {
+            // handle response
+            var arr = response.courses.completed;
+            for(var i=0; i < arr.lenght; i++){
+                $('<div>Appended item</div>',{
+                    'class'    : 'course'
+                }).appendTo('#badges')
+                    .append('<h3>title</h3>')
+                    .append($('<img/>', {
+                        'scr'  : arr,
+                    })
+                    .apppend($('<a/>', {
+                        'class' : 'btn btn-primary',
+                        'href'  : 'courses url property',
+                        'target': '_blank'                        
+                    })
+                );
+            };           
         }
     });
-
 });
