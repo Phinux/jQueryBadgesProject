4095128828cd713a930d5ffb7e1036aea4bec4a6
atoonen <atoonen@NB-ATOONEN>
2017-05-11 21:25:07 +0200

    eerste poging

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
-  // your code will go here
+    // your code will go here
+    
+
+    //Make an Ajax request to the Report Card URL you found on CodeSchool.com - Set the dataType as jsonp - Pass a success callback We made a sample Ajax request available to help you with this task.
+
+    $.ajax({
+        url: "https://www.codeschool.com/users/Phinux", success: function (result) {
+
+
+        }
+    });
 
 });
